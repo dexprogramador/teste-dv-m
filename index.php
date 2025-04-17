b5287f0dd1ca720f766ff2eb353096fb73aeb594
dexprogramador <mauriciospeedsys@gmail.com>
2025-04-16 22:55:34 -0300

    Agora sim, index atualizado da pasta certa

--- index.php
+++ index.php
@@ -1,180 +1,85 @@
-    <?php include('partials-front/menu.php'); ?>
+<?php include('partials-front/menu.php'); ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
-                <input type="search" name="search" placeholder="Search for Food.." required>
-                <input type="submit" name="submit" value="Search" class="btn btn-primary">
-            </form>
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+    <div class="container">
+        <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
+            <input type="search" name="search" placeholder="Buscar produto..." required>
+            <input type="submit" name="submit" value="Buscar" class="btn btn-primary">
+        </form>
+    </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
 
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+<?php
+if (isset($_SESSION['order'])) {
+    echo $_SESSION['order'];
+    unset($_SESSION['order']);
+}
+?>
 
-    <?php 
-        if(isset($_SESSION['order']))
-        {
-            echo $_SESSION['order'];
-            unset($_SESSION['order']);
-        }
-    ?>
+<!-- PRODUTOS AGRUPADOS POR CATEGORIA -->
+<section class="food-menu">
+    <div class="container">
+        <h2 class="text-center">Escolha seu pedido na BK Lounge</h2>
 
-    <!-- CAtegories Section Starts Here -->
-    <section class="categories">
-        <div class="container">
-            <h2 class="text-center">Explore Foods</h2>
+        <?php
+        // Buscar categorias ativas
+        $sql_cat = "SELECT * FROM tbl_category WHERE active='Yes' ORDER BY title ASC";
+        $res_cat = mysqli_query($conn, $sql_cat);
 
-            <?php 
-                //Create SQL Query to Display CAtegories from Database
-                $sql = "SELECT * FROM tbl_category WHERE active='Yes' AND featured='Yes' LIMIT 3";
-                //Execute the Query
-                $res = mysqli_query($conn, $sql);
-                //Count rows to check whether the category is available or not
-                $count = mysqli_num_rows($res);
+        if (mysqli_num_rows($res_cat) > 0) {
+            while ($cat = mysqli_fetch_assoc($res_cat)) {
+                $cat_id = $cat['id'];
+                $cat_title = $cat['title'];
+                echo "<h3 style='margin-top:40px; margin-bottom:15px; font-size:24px; font-weight:bold; color:#333;'>" . $cat_title . "</h3>";
 
-                if($count>0)
-                {
-                    //CAtegories Available
-                    while($row=mysqli_fetch_assoc($res))
-                    {
-                        //Get the Values like id, title, image_name
-                        $id = $row['id'];
-                        $title = $row['title'];
-                        $image_name = $row['image_name'];
-                        ?>
-                        
-                        <a href="<?php echo SITEURL; ?>category-foods.php?category_id=<?php echo $id; ?>">
-                            <div class="box-3 float-container">
-                                <?php 
-                                    //Check whether Image is available or not
-                                    if($image_name=="")
-                                    {
-                                        //Display MEssage
-                                        echo "<div class='error'>Image not Available</div>";
-                                    }
-                                    else
-                                    {
-                                        //Image Available
-                                        ?>
-                                        <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="Pizza" class="img-responsive img-curve">
-                                        <?php
-                                    }
+                // Buscar produtos dessa categoria
+                $sql_food = "SELECT * FROM tbl_food WHERE active='Yes' AND category_id=$cat_id ORDER BY title ASC";
+                $res_food = mysqli_query($conn, $sql_food);
+
+                if (mysqli_num_rows($res_food) > 0) {
+                    while ($food = mysqli_fetch_assoc($res_food)) {
+                        $food_id = $food['id'];
+                        $food_title = $food['title'];
+                        $food_price = $food['price'];
+                        $food_description = $food['description'];
+                        $food_image = $food['image_name'];
+        ?>
+
+                        <div class="food-menu-box">
+                            <div class="food-menu-img">
+                                <?php
+                                if ($food_image == "") {
+                                    echo "<div class='error'>Imagem não disponível</div>";
+                                } else {
+                                    echo "<img src='" . SITEURL . "images/food/" . $food_image . "' alt='" . $food_title . "' class='img-responsive img-curve'>";
+                                }
                                 ?>
-                                
-
-                                <h3 class="float-text text-white"><?php echo $title; ?></h3>
                             </div>
-                        </a>
 
-                        <?php
+                            <div class="food-menu-desc">
+                                <h4><?php echo $food_title; ?></h4>
+                                <p class="food-price">R$ <?php echo number_format($food_price, 2, ',', '.'); ?></p>
+                                <p class="food-detail"><?php echo $food_description; ?></p>
+                                <br>
+                                <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $food_id; ?>" class="btn btn-primary">Fazer Pedido</a>
+                            </div>
+                        </div>
+
+        <?php
                     }
-                }
-                else
-                {
-                    //Categories not Available
-                    echo "<div class='error'>Category not Added.</div>";
-                }
-            ?>
-
-
-            <div class="clearfix"></div>
-        </div>
-    </section>
-    <!-- Categories Section Ends Here -->
-
-
-
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Featured Foods</h2>
-
-            <?php 
-            
-            //Getting Foods from Database that are active and featured
-            //SQL Query
-            $sql2 = "SELECT * FROM tbl_food WHERE active='Yes' AND featured='Yes' LIMIT 6";
-
-            //Execute the Query
-            $res2 = mysqli_query($conn, $sql2);
-
-            //Count Rows
-            $count2 = mysqli_num_rows($res2);
-
-            //CHeck whether food available or not
-            if($count2>0)
-            {
-                //Food Available
-                while($row=mysqli_fetch_assoc($res2))
-                {
-                    //Get all the values
-                    $id = $row['id'];
-                    $title = $row['title'];
-                    $price = $row['price'];
-                    $description = $row['description'];
-                    $image_name = $row['image_name'];
-                    ?>
-
-                    <div class="food-menu-box">
-                        <div class="food-menu-img">
-                            <?php 
-                                //Check whether image available or not
-                                if($image_name=="")
-                                {
-                                    //Image not Available
-                                    echo "<div class='error'>Image not available.</div>";
-                                }
-                                else
-                                {
-                                    //Image Available
-                                    ?>
-                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                                    <?php
-                                }
-                            ?>
-                            
-                        </div>
-
-                        <div class="food-menu-desc">
-                            <h4><?php echo $title; ?></h4>
-                            <p class="food-price">₹<?php echo $price; ?></p>
-                            <p class="food-detail">
-                                <?php echo $description; ?>
-                            </p>
-                            <br>
-
-                            <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
-                        </div>
-                    </div>
-
-                    <?php
+                } else {
+                    echo "<p class='error'>Nenhum produto encontrado nesta categoria.</p>";
                 }
             }
-            else
-            {
-                //Food Not Available 
-                echo "<div class='error'>Food not available.</div>";
-            }
-            
-            ?>
+        } else {
+            echo "<p class='error'>Nenhuma categoria cadastrada.</p>";
+        }
+        ?>
 
-            
+        <div class="clearfix"></div>
+    </div>
+</section>
 
- 
-
-            <div class="clearfix"></div>
-
-            
-
-        </div>
-
-        <p class="text-center">
-            <a href="http://localhost/food-order/foods.php">See All Foods</a>
-        </p>
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
-
-    
-    <?php include('partials-front/footer.php'); ?>
+<?php include('partials-front/footer.php'); ?>
